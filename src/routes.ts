7bf64c8bb0f875ffe6432bf7ef093b52ed8d332b
Pedro Andrade <pedroandrade20xx@gmail.com>
2022-08-31 15:47:58 -0300

    feat: add ListBillItemController 'get' route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
+import { ListBillItemController } from "./controller/ListBillItemController";
 import { Router, Request, Response } from "express";
 
 export const router = Router();
+
+const listBillItemController = new ListBillItemController();
+router.get('/bill-items', listBillItemController.handle);
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({ message: "The API is running..." });
