a6ebf188c0e0eb2c6aa672d9221a652987f7560a
Pedro Andrade <pedroandrade20xx@gmail.com>
2022-08-31 16:10:36 -0300

    feat: add CreateBillItemController 'post' route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
+import { CreateBillItemController } from "./controller/CreateBillItemController";
 import { ListBillItemController } from "./controller/ListBillItemController";
 import { Router, Request, Response } from "express";
 
 export const router = Router();
+
+const createBillItemController = new CreateBillItemController();
+router.post('/bill-items', createBillItemController.handle);
 
 const listBillItemController = new ListBillItemController();
 router.get('/bill-items', listBillItemController.handle);
